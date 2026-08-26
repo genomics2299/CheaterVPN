c2694e0098257915d86cd0bca048a49fd2aad3dd
genomics2299 <genomics2299@users.noreply.github.com>
2026-08-26 20:39:46 +0300

    v1.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cheatervpnapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.25"
+        versionCode = 27
+        versionName = "1.26"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
