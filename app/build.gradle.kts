cde778b25f998d157b4ab37eb897ad0aef95081b
genomics2299 <genomics2299@users.noreply.github.com>
2026-08-26 18:48:36 +0300

    v1.9: replace statistics button with icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cheatervpnapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
