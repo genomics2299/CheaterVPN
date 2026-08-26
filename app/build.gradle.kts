7308aaa2be8f8835ae6cb12da92aac33bb4506a4
genomics2299 <genomics2299@users.noreply.github.com>
2026-08-26 19:25:07 +0300

    v1.15: internet icon for contact, all 3 icons in one row

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cheatervpnapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.14"
+        versionCode = 16
+        versionName = "1.15"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
