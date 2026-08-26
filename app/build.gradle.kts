36aba7d2a62fbae4172cbdfabcd4d2e2c6cc071b
genomics2299 <genomics2299@users.noreply.github.com>
2026-08-26 18:31:48 +0300

    v1.7: updated back icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cheatervpnapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
