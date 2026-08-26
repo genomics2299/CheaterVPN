0d9d3006bbc193b424bfe0e8f3f56176b470c4f8
genomics2299 <genomics2299@users.noreply.github.com>
2026-08-26 19:35:03 +0300

    v1.16: replace internet icon with contact icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cheatervpnapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.15"
+        versionCode = 17
+        versionName = "1.16"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
